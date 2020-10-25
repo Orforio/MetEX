40cd45a43294b937c6c88efc231a7e6502a5df17
Richard Whittaker <pkeruno@gmail.com>
2020-10-25 22:46:44 +0000

    Add Scully Sitemap plugin

--- scully.metex.config.ts
+++ scully.metex.config.ts
@@ -1,7 +1,46 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 
 const Http404Plugin = getHttp404Plugin();
+const SitemapPlugin = getSitemapPlugin();
+
+setPluginConfig(SitemapPlugin, {
+	urlPrefix: 'https://www.metex.app',
+	sitemapFilename: 'sitemap.xml',
+	changeFreq: 'monthly',
+	priority: [
+		'1.0',
+		'0.9',
+		'0.8',
+		'0.7',
+		'0.6',
+		'0.5',
+		'0.4',
+		'0.3',
+		'0.2',
+		'0.1',
+		'0.0',
+	],
+	ignoredRoutes: ['/404'],
+	routes: {
+		'/lines/:slug': {
+			changeFreq: 'monthly',
+			priority: '0.9',
+			sitemapFilename: 'sitemap-lines.xml',
+		},
+		'/lines/:linesSlug/:stationSlug': {
+			changeFreq: 'monthly',
+			priority: '0.8',
+			sitemapFilename: 'sitemap-lines-stations.xml',
+		},
+		'/places/:slug': {
+			changeFreq: 'monthly',
+			priority: '0.9',
+			sitemapFilename: 'sitemap-places.xml',
+		}
+	}
+});
 
 export const config: ScullyConfig = {
 	projectRoot: './src',
