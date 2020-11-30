afab008a83e44acb8ec4836f4a80ea33c0bb213b
Richard Whittaker <pkeruno@gmail.com>
2020-11-30 15:55:53 +0000

    Initial PWA setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule, Title } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 import { ScullyLibModule } from '@scullyio/ng-lib';
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { SharedModule } from './shared/shared.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [
@@ -26,6 +28,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 		MarkdownModule.forRoot(),
 		NgbCollapseModule,
 		ScullyLibModule,
+		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
 		SharedModule
 	],
 	providers: [Title],
